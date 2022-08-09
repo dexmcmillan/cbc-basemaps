70f067bf961d1f2978ef0c0b7bbcaf1030de6fde
dexmcmillan <dexmcmillan@gmail.com>
2022-08-09 08:25:43 -0400

    new tests for map uploads to dw

--- basemap_upload_test.py
+++ basemap_upload_test.py
@@ -6,21 +6,22 @@ with open('./auth.txt', 'r') as f:
         DW_AUTH_TOKEN = f.read().strip()
 
 # Open up the geojson I want to load as the base map.
-with open("./data/cities/sk-regina-census_tracts.geojson") as jsonFile:
-    jsonObject = json.load(jsonFile)
-    jsonObject["type"] = "Topology"
-    jsonFile.close()
+with open("bc-kelowna-census_tracts.json") as f:
+    jsonObject = json.load(f)
 
 
 # Define headers for request below.
 headers = {
-    "Accept": "*/*",
     "Authorization": f"Bearer {DW_AUTH_TOKEN}",
     "Content-Type": "application/json"
 }
 
-id = "whDn4"
+id = "ce2uC"
 
 r = requests.put(f"https://api.datawrapper.de/v3/charts/{id}/assets/{id}.map.json", headers=headers, json=json.dumps(jsonObject))
 
 print(r)
+
+chart = requests.get(f"https://api.datawrapper.de/v3/charts/{id}", headers=headers)
+
+print(chart.json()["metadata"]['visualize'])
